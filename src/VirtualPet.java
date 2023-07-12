6b1e6428435ba05bcf5c886476f7e9da393d8002
SirHertzAlot <Dukjoseph@gmail.com>
2023-07-12 01:21:51 -0400

    "Added Virtual Pet class, started on tick method. Added hunger, thirst, waste, boredom, sickness, and will be adding tired as well to pet constructor with fields. Will be working with Timer and TimerTask in main game loop to build game."

---
+++ src/VirtualPet.java
@@ -0,0 +1,133 @@
+import java.util.*;
+
+public class VirtualPet {
+
+    Timer timer = new Timer();
+    TimerTask task;
+    int hungerLvl = 0; //0 is starting point, not hungry. 5 is end point, and very hungry!!
+    boolean isHungry = false;
+    int feedMe = 2;
+    int thirstLvl = 0;
+    int waterMe = 1;
+    boolean isThirsty = false;
+    boolean waste = false;
+    int boredomLvl = 0; //0 is not thirsty. 5 is very thirsty!!
+    int playWithMe = 2;
+    boolean isBored = false;
+    boolean sickness = false;
+    int time;
+    
+    public VirtualPet(boolean eat, boolean drink, boolean needBathroom, boolean wantToPlay, boolean sickness){
+        this.isHungry = eat;
+        this.isThirsty = drink;
+        this.isBored = wantToPlay;
+        this.waste = needBathroom;
+        this.sickness = sickness;
+        task = new TimerTask(){
+            public void run(){
+
+            }
+
+        };
+    }
+    public int getHungerLvl(){
+        return hungerLvl;
+    }
+
+    public int getThirstLvl(){
+        return thirstLvl;
+    }
+
+    public int getBoredLvl(){
+        return boredomLvl;
+    }
+
+    public boolean timeToEat(int hungerLvl) {
+        if(getHungerLvl() == 1){
+            isHungry = true;
+            System.out.println("looks like your pet is hungry! Hunger level is: " + hungerLvl);
+        } else if(getHungerLvl() > 1 && getHungerLvl() < 4)  {
+            isHungry = true;
+            System.out.println("Your pet is starving!! Hunger level is: " + hungerLvl);
+        } else if (getHungerLvl() == 5){
+            isHungry = false;
+            System.out.println("Your pet is dead. You didn't feed it in time, sorry.. Hunger level is: " + hungerLvl);
+        }
+
+        return isHungry;
+    }
+
+    public int eat(){
+        if(timeToEat(hungerLvl)){
+            hungerLvl -= feedMe;
+        } else {
+            hungerLvl = hungerLvl;
+        }
+        return hungerLvl;
+    }
+
+    public int drink(){
+        if(timeToDrink(thirstLvl)){
+            thirstLvl -= waterMe;
+        } else {
+            thirstLvl = thirstLvl;
+        }
+        return thirstLvl;
+    }
+
+    public int play(){
+        if(timeToPlay(boredomLvl)){
+            boredomLvl -= playWithMe;
+            playWithMe += thirstLvl;
+        } else {
+            boredomLvl = boredomLvl;
+        }
+        return boredomLvl;
+    }
+
+    public boolean timeToDrink(int thirstLvl) {
+        if(getThirstLvl() == 1){
+            isThirsty = true;
+            System.out.println("looks like your pet is thirsty! Thirst level is: " + thirstLvl);
+        } else if(getThirstLvl() > 1 && getThirstLvl() < 4)  {
+            isThirsty = true;
+            System.out.println("Your pet is very thirsty!! Thirst level is: " + thirstLvl);
+        } else if (getThirstLvl() == 5){
+            isThirsty = false;
+            System.out.println("Your pet is dead. It died of dehydration.. Thirst level is: " + thirstLvl);
+        }
+
+        return isThirsty;
+    }
+
+    public boolean timeToPlay(int boredomLvl) {
+        if(getBoredLvl() == 1){
+            isBored = true;
+            System.out.println("Looks like your pet is bored! Boredom level is: " + boredomLvl);
+        } else if(getBoredLvl() > 1 && getBoredLvl() < 4)  {
+            isBored = true;
+            System.out.println("Your pet is so bored it's reading!! Boredom level is: " + boredomLvl);
+        } else if (getBoredLvl() == 5){
+            isBored = false;
+            System.out.println("Your pet is dead. It died of boredom, sorry.. Boredom level is: " + boredomLvl);
+        }
+        return isBored;
+    }
+
+    public boolean timeToPotty() {
+        if(!timeToEat(hungerLvl) && !timeToPlay(boredomLvl)){
+            waste = true;
+            System.out.println("Oh, it looks like your pet is ready to use the bathroom!");
+        } else {
+            waste = false;
+            System.out.println("Your pet isn't ready to use the potty yet.");
+        }
+        return waste;
+    }
+
+    public int tick(){
+
+
+        return time;
+    }
+}
--- test/VirtualPetTest.java
+++ test/VirtualPetTest.java
@@ -1,5 +1,14 @@
-import org.junit.*;
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 public class VirtualPetTest {
-    
+
+    @Test
+    public void timeToEat(int eat){
+        VirtualPet falcon = new VirtualPet(false, false, false, false, false);
+        falcon.timeToEat(1);
+
+        boolean check = falcon.isHungry;
+        assertEquals(true, falcon.isHungry);
+    }
 }
